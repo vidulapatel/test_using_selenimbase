9dbf22247669f0d9a2de7707b5b13a661e82c3b7
vidulapatel <vidulasabnis@gmail.com>
2020-05-18 01:34:55 -0400

    correcting build and test run files

--- src/build_deploy.sh
+++ src/build_deploy.sh
@@ -4,7 +4,7 @@ set -e
 pip3 install -r 'requirements.txt'
 git clone https://github.com/seleniumbase/SeleniumBase.git
 cd SeleniumBase/
-pip install -r requirements.txt
-python setup.py install
+pip3 install -r requirements.txt
+python3 setup.py install
 
 seleniumbase install chromedriver
--- src/run_test.sh
+++ src/run_test.sh
@@ -1,12 +1,12 @@
 set -e
 
-# virtualenv pip3_test_env
+virtualenv pip3_test_env
 . ./pip3_test_env/bin/activate
 
 # Install Python dependencies
 pip3 install -r 'requirements.txt'
 
-cp test_verify_temp.py SeleniumBase/examples/test_event_firing.py
+cp test_verify_temp_hours.py SeleniumBase/examples/test_event_firing.py
 
 cd SeleniumBase/examples/
 
